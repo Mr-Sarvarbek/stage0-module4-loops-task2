27f2df405625713488fba7ee1fad3e04c5b1a813
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-04 22:42:16 +0500

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int count=0;
+        int P=1;
+        while(count!=power){
+         ;
 
     }
 }
