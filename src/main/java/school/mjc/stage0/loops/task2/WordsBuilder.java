105f1b3bc6dca0c8d8a5b03f80bf3553655e80cd
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-05 21:29:34 +0500

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.loops.task2;
 
-public class WordsBuilder {
-    public void buildPhrase(char... chars) {
+import school.mjc.stage0.base.BaseIOTest;
+import org.junit.jupiter.api.Test;
 
+class WordsBuilderTest extends BaseIOTest {
+
+//     @Test
+    void buildPhraseBuildsAPhrase() {
+        WordsBuilder builder = new WordsBuilder();
+
+        builder.buildPhrase('l', 'o', 'o', 'p', 's', ' ', 'a', 'r', 'e', ' ', 'c', 'o', 'o', 'l');
+
+        assertOutEquals("loops are cool");
     }
 }
